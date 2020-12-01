78a063918d4cd91a8330be83200d54ee2d6ffbc4
Koob <74259369+Koob-tcc@users.noreply.github.com>
2020-12-01 15:00:20 -0300

    Update Cadastro.php

--- class/Cadastro.php
+++ class/Cadastro.php
@@ -12,12 +12,21 @@ class Cadastro extends Base
 	private $email;
 	private $senha;
 
+	private $compara_senha = false;
+	private $compara_email = false;
+
 	public function usuarios(){
 		$consulta = "SELECT * FROM `$this->tabela`";
 		$resultado = Parent::pegar_dados($consulta);
 		return $resultado;
 	}
 
+	public function usuario_logado($email){
+		$consulta = "SELECT * FROM `$this->tabela` WHERE email = ?";
+		$par = [$email];
+		$resultado = Parent::pegar_dados($consulta,$par);
+		return $resultado;
+	}
 	public function cadastrar($nome, $sobrenome, $email, $senha){
 		$consulta = "INSERT INTO `$this->tabela` VALUES(null,?,?,?,?)";
 		$this->nome = $nome;
@@ -26,6 +35,34 @@ class Cadastro extends Base
 		$this->senha = $senha;
 		$par = [$this->nome, $this->sobrenome, $this->email, $this->senha];
 		$resultado = Parent::novos_dados($consulta,$par);
+	}
+
+	public function login($email, $senha){
+		$consulta = "SELECT nome, email, senha FROM `$this->tabela` WHERE email = ?";
+		$par = [$email];
+		$resultado = Parent::pegar_um($consulta, $par);
+		if($resultado){
+			$this->compara_email = true;
+			if($senha === $resultado['senha']){
+				$this->compara_senha = true;
+				$_SESSION['login']=true;
+				$_SESSION['nome']=$resultado['nome'];
+			}
+		}
+		$resultado = self::resultado_login();
+		return $resultado;
+		die();
+	}
+
+	public function sair(){
+		session_destroy();
+	}
+
+	private function resultado_login(){
+		if($this->compara_senha && $this->compara_email)
+			return true;
+		else
+			return false;
 	}
 }
 ?>
